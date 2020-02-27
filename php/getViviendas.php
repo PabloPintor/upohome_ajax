ba4ef01e3ccfcce20f0e4618fe261816b06c1837
PabloPintor <pablo@pablopintor.com>
2020-02-27 17:10:37 +0100

    php get

---
+++ php/getViviendas.php
@@ -0,0 +1,12 @@
+<?php
+$conexion = mysqli_connect("localhost","root","","upohome");
+$conexion->set_charset("utf8");
+
+$SQL = "SELECT * FROM viviendas";
+$resultado = $conexion->query($SQL);
+$datos = [];
+while($row = $resultado -> fetch_assoc()){ //fetch_array
+    $datos[] = $row;
+}
+echo json_encode($datos);
+?>
